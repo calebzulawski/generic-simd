523b3e00a114ea5c7c383a7eb753119aa9763669
Caleb Zulawski <caleb.zulawski@gmail.com>
2020-05-27 23:21:48 -0400

    Switch to recursive shim definition

--- safe-simd/src/shim.rs
+++ safe-simd/src/shim.rs
@@ -4,176 +4,164 @@ use crate::vector::{Complex, Vector};
 use arch_types::marker::Superset;
 use core::marker::PhantomData;
 
+/// Shim that doubles the width of a vector.
+#[derive(Copy, Clone, Debug)]
+pub struct Shim2<Underlying, Scalar>([Underlying; 2], PhantomData<Scalar>);
+
+/// Shim that quadruples the width of a vector.
+pub type Shim4<Underlying, Scalar> = Shim2<Shim2<Underlying, Scalar>, Scalar>;
+
+/// Shim that octuples the width of a vector.
+pub type Shim8<Underlying, Scalar> = Shim4<Shim2<Underlying, Scalar>, Scalar>;
+
+unsafe impl<Underlying, Scalar> Vector for Shim2<Underlying, Scalar>
+where
+    Underlying: Vector<Scalar = Scalar>,
+    Scalar: Copy,
+{
+    type Scalar = Scalar;
+    type Feature = <Underlying as Vector>::Feature;
+
+    #[inline]
+    fn splat(feature: impl Superset<Self::Feature>, from: Self::Scalar) -> Self {
+        Self([Underlying::splat(feature, from); 2], PhantomData)
+    }
+}
+
+impl<Underlying, Scalar> AsRef<[Scalar]> for Shim2<Underlying, Scalar>
+where
+    Underlying: Vector<Scalar = Scalar>,
+    Scalar: Copy,
+{
+    #[inline]
+    fn as_ref(&self) -> &[Scalar] {
+        self.as_slice()
+    }
+}
+
+impl<Underlying, Scalar> AsMut<[Scalar]> for Shim2<Underlying, Scalar>
+where
+    Underlying: Vector<Scalar = Scalar>,
+    Scalar: Copy,
+{
+    #[inline]
+    fn as_mut(&mut self) -> &mut [Scalar] {
+        self.as_slice_mut()
+    }
+}
+
+impl<Underlying, Scalar> core::ops::Deref for Shim2<Underlying, Scalar>
+where
+    Underlying: Vector<Scalar = Scalar>,
+    Scalar: Copy,
+{
+    type Target = [Scalar];
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.as_slice()
+    }
+}
+
+impl<Underlying, Scalar> core::ops::DerefMut for Shim2<Underlying, Scalar>
+where
+    Underlying: Vector<Scalar = Scalar>,
+    Scalar: Copy,
+{
+    #[inline]
+    fn deref_mut(&mut self) -> &mut <Self as core::ops::Deref>::Target {
+        self.as_slice_mut()
+    }
+}
+
 macro_rules! implement {
     {
-        $type:ident, $size_lit:literal, $size_string:literal
+        @op $trait:ident :: $func:ident
     } => {
-        #[doc = "Shim for a vector of "]
-        #[doc = $size_string]
-        #[doc = " native vectors"]
-        #[derive(Copy, Clone, Debug)]
-        pub struct $type<Underlying, Scalar>([Underlying; $size_lit], PhantomData<Scalar>);
-
-        unsafe impl<Underlying, Scalar> Vector for $type<Underlying, Scalar>
-        where
-            Underlying: Vector<Scalar = Scalar>,
-            Scalar: Copy,
-        {
-            type Scalar = Scalar;
-            type Feature = <Underlying as Vector>::Feature;
-
-            #[inline]
-            fn splat(feature: impl Superset<Self::Feature>, from: Self::Scalar) -> Self {
-                Self([Underlying::splat(feature, from); $size_lit], PhantomData)
-            }
-        }
-
-        impl<Underlying, Scalar> AsRef<[Scalar]> for $type<Underlying, Scalar>
-        where
-            Underlying: Vector<Scalar = Scalar>,
-            Scalar: Copy,
-        {
-            fn as_ref(&self) -> &[Scalar] {
-                self.as_slice()
-            }
-        }
-
-        impl<Underlying, Scalar> AsMut<[Scalar]> for $type<Underlying, Scalar>
-        where
-            Underlying: Vector<Scalar = Scalar>,
-            Scalar: Copy,
-        {
-            fn as_mut(&mut self) -> &mut [Scalar] {
-                self.as_slice_mut()
-            }
-        }
-
-        impl<Underlying, Scalar> core::ops::Deref for $type<Underlying, Scalar>
-        where
-            Underlying: Vector<Scalar = Scalar>,
-            Scalar: Copy,
-        {
-            type Target = [Scalar];
-            fn deref(&self) -> &Self::Target {
-                self.as_slice()
-            }
-        }
-
-        impl<Underlying, Scalar> core::ops::DerefMut for $type<Underlying, Scalar>
-        where
-            Underlying: Vector<Scalar = Scalar>,
-            Scalar: Copy,
-        {
-            fn deref_mut(&mut self) -> &mut <Self as core::ops::Deref>::Target {
-                self.as_slice_mut()
-            }
-        }
-
-        implement! { @op $type => Add::add }
-        implement! { @op $type => Sub::sub }
-        implement! { @op $type => Mul::mul }
-        implement! { @op $type => Div::div }
-        implement! { @op_assign $type => AddAssign::add_assign }
-        implement! { @op_assign $type => SubAssign::sub_assign }
-        implement! { @op_assign $type => MulAssign::mul_assign }
-        implement! { @op_assign $type => DivAssign::div_assign }
-
-        impl<Underlying, Scalar> core::ops::Neg for $type<Underlying, Scalar>
-        where
-            Underlying: Copy + core::ops::Neg<Output=Underlying>,
-        {
-            type Output = Self;
-            fn neg(mut self) -> Self {
-                for x in self.0.iter_mut() {
-                    *x = -*x;
-                }
-                self
-            }
-        }
-
-        impl<Underlying, Real> Complex for $type<Underlying, num_complex::Complex<Real>>
-        where
-            Underlying: Vector<Scalar = num_complex::Complex<Real>> + Complex<RealScalar = Real>,
-            Real: Copy,
-        {
-            type RealScalar = Real;
-
-            fn mul_i(mut self) -> Self {
-                for x in self.0.iter_mut() {
-                    *x = x.mul_i()
-                }
-                self
-            }
-
-            fn mul_neg_i(mut self) -> Self {
-                for x in self.0.iter_mut() {
-                    *x = x.mul_neg_i()
-                }
-                self
-            }
-        }
-    };
-
-    {
-        @op $type:ident => $trait:ident :: $func:ident
-    } => {
-        impl<Underlying, Scalar> core::ops::$trait<Self> for $type<Underlying, Scalar>
+        impl<Underlying, Scalar> core::ops::$trait<Self> for Shim2<Underlying, Scalar>
         where
             Underlying: Copy + core::ops::$trait<Underlying, Output=Underlying>,
         {
             type Output = Self;
-            fn $func(mut self, rhs: Self) -> Self {
-                for (a, b) in self.0.iter_mut().zip(rhs.0.iter()) {
-                    *a = a.$func(*b);
-                }
-                self
+            fn $func(self, rhs: Self) -> Self {
+                Self([self.0[0].$func(rhs.0[0]), self.0[1].$func(rhs.0[1])], PhantomData)
             }
         }
 
-        impl<Underlying, Scalar> core::ops::$trait<Scalar> for $type<Underlying, Scalar>
+        impl<Underlying, Scalar> core::ops::$trait<Scalar> for Shim2<Underlying, Scalar>
         where
             Underlying: Copy + core::ops::$trait<Scalar, Output=Underlying>,
             Scalar: Copy,
         {
             type Output = Self;
-            fn $func(mut self, rhs: Scalar) -> Self {
-                for x in self.0.iter_mut() {
-                    *x = x.$func(rhs);
-                }
-                self
+            fn $func(self, rhs: Scalar) -> Self {
+                Self([self.0[0].$func(rhs), self.0[1].$func(rhs)], PhantomData)
             }
         }
     };
 
     {
-        @op_assign $type:ident => $trait:ident :: $func:ident
+        @op_assign $trait:ident :: $func:ident
     } => {
-        impl<Underlying, Scalar> core::ops::$trait<Self> for $type<Underlying, Scalar>
+        impl<Underlying, Scalar> core::ops::$trait<Self> for Shim2<Underlying, Scalar>
         where
             Underlying: Copy + core::ops::$trait<Underlying>,
             Scalar: Copy,
         {
             fn $func(&mut self, rhs: Self) {
-                for (a, b) in self.0.iter_mut().zip(rhs.0.iter()) {
-                    a.$func(*b);
-                }
+                self.0[0].$func(rhs.0[0]);
+                self.0[1].$func(rhs.0[1]);
             }
         }
 
-        impl<Underlying, Scalar> core::ops::$trait<Scalar> for $type<Underlying, Scalar>
+        impl<Underlying, Scalar> core::ops::$trait<Scalar> for Shim2<Underlying, Scalar>
         where
             Underlying: Copy + core::ops::$trait<Scalar>,
             Scalar: Copy,
         {
             fn $func(&mut self, rhs: Scalar) {
-                for x in self.0.iter_mut() {
-                    x.$func(rhs);
-                }
+                self.0[0].$func(rhs);
+                self.0[1].$func(rhs);
             }
         }
     };
 }
 
-implement! { Shim2, 2, "2" }
-implement! { Shim4, 4, "4" }
-implement! { Shim8, 8, "8" }
+implement! { @op Add::add }
+implement! { @op Sub::sub }
+implement! { @op Mul::mul }
+implement! { @op Div::div }
+implement! { @op_assign AddAssign::add_assign }
+implement! { @op_assign SubAssign::sub_assign }
+implement! { @op_assign MulAssign::mul_assign }
+implement! { @op_assign DivAssign::div_assign }
+
+impl<Underlying, Scalar> core::ops::Neg for Shim2<Underlying, Scalar>
+where
+    Underlying: Copy + core::ops::Neg<Output = Underlying>,
+{
+    type Output = Self;
+
+    #[inline]
+    fn neg(self) -> Self {
+        Self([-self.0[0], -self.0[1]], PhantomData)
+    }
+}
+
+impl<Underlying, Real> Complex for Shim2<Underlying, num_complex::Complex<Real>>
+where
+    Underlying: Vector<Scalar = num_complex::Complex<Real>> + Complex<RealScalar = Real>,
+    Real: Copy,
+{
+    type RealScalar = Real;
+
+    #[inline]
+    fn mul_i(self) -> Self {
+        Self([self.0[0].mul_i(), self.0[1].mul_i()], PhantomData)
+    }
+
+    #[inline]
+    fn mul_neg_i(self) -> Self {
+        Self([self.0[0].mul_neg_i(), self.0[1].mul_neg_i()], PhantomData)
+    }
+}
