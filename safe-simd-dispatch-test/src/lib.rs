00351a177d3c50448653b33806d0c96f607c2082
Caleb Zulawski <caleb.zulawski@gmail.com>
2020-05-27 23:26:59 -0400

    Fix clippy lint

--- safe-simd-dispatch-test/src/lib.rs
+++ safe-simd-dispatch-test/src/lib.rs
@@ -7,12 +7,7 @@ pub fn add_one_aligned(x: &mut [f32]) {
         *s += 1.;
     }
 
-    let ones = feature.splat_native(1f32);
     for v in vecs {
-        *v = *v + ones;
-    }
-
-    for s in x.iter() {
-        assert_eq!(*s, 1f32);
+        *v += 1.;
     }
 }
