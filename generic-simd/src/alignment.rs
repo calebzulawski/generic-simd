d07375c0eb368e6197f95caf89503ae53a73ef61
Caleb Zulawski <caleb.zulawski@gmail.com>
2020-08-14 18:11:33 -0400

    Rework alignment

--- generic-simd/src/alignment.rs
+++ generic-simd/src/alignment.rs
@@ -11,199 +11,59 @@ use crate::{
     vector::{scalar, width, SizedVector},
 };
 
-/// A zero sized type with the same alignment as `T`.
-pub struct TypeAlignment<T> {
-    _t: [T; 0],
-}
-
-unsafe impl<T> Alignment for TypeAlignment<T> {}
-
-impl<T: Copy> Copy for TypeAlignment<T> {}
-
-impl<T> Clone for TypeAlignment<T> {
-    fn clone(&self) -> Self {
-        Self::default()
-    }
-}
-
-impl<T> Default for TypeAlignment<T> {
-    fn default() -> Self {
-        Self { _t: [] }
-    }
-}
-
-impl<T> core::fmt::Debug for TypeAlignment<T> {
-    #[inline]
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.pad("TypeAlignment")
-    }
-}
-
-impl<T> core::cmp::PartialEq for TypeAlignment<T> {
-    #[inline]
-    fn eq(&self, _other: &Self) -> bool {
-        true
-    }
-}
-
-impl<T> core::cmp::Eq for TypeAlignment<T> {}
-
-impl<T> core::cmp::PartialOrd for TypeAlignment<T> {
-    #[inline]
-    fn partial_cmp(&self, _other: &Self) -> Option<core::cmp::Ordering> {
-        Some(core::cmp::Ordering::Equal)
-    }
-}
-
-impl<T> core::cmp::Ord for TypeAlignment<T> {
-    #[inline]
-    fn cmp(&self, _other: &Self) -> core::cmp::Ordering {
-        core::cmp::Ordering::Equal
-    }
-}
-
-impl<T> core::hash::Hash for TypeAlignment<T> {
-    #[inline]
-    fn hash<H: core::hash::Hasher>(&self, _: &mut H) {}
-}
-
-/// A zero sized type with vector alignment for a particular token and scalar.
 #[repr(C)]
-pub struct TokenAlignment<Token: arch::Token, Scalar: scalar::Scalar<Token>> {
-    _1: TypeAlignment<SizedVector<Scalar, width::W1, Token>>,
-    _2: TypeAlignment<SizedVector<Scalar, width::W2, Token>>,
-    _4: TypeAlignment<SizedVector<Scalar, width::W4, Token>>,
-    _8: TypeAlignment<SizedVector<Scalar, width::W8, Token>>,
-}
-
-unsafe impl<Token: arch::Token, Scalar: scalar::Scalar<Token>> Alignment
-    for TokenAlignment<Token, Scalar>
-{
-}
-
-impl<Token: arch::Token, Scalar: scalar::Scalar<Token>> Copy for TokenAlignment<Token, Scalar> {}
-
-impl<Token: arch::Token, Scalar: scalar::Scalar<Token>> Clone for TokenAlignment<Token, Scalar> {
-    fn clone(&self) -> Self {
-        Self::default()
-    }
-}
-
-impl<Token: arch::Token, Scalar: scalar::Scalar<Token>> core::fmt::Debug
-    for TokenAlignment<Token, Scalar>
-{
-    #[inline]
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.pad("TokenAlignment")
-    }
-}
-
-impl<Token: arch::Token, Scalar: scalar::Scalar<Token>> Default for TokenAlignment<Token, Scalar> {
-    fn default() -> Self {
-        Self {
-            _1: Default::default(),
-            _2: Default::default(),
-            _4: Default::default(),
-            _8: Default::default(),
-        }
-    }
-}
-
-impl<Token: arch::Token, Scalar: scalar::Scalar<Token>> core::cmp::PartialEq
-    for TokenAlignment<Token, Scalar>
-{
-    #[inline]
-    fn eq(&self, _other: &Self) -> bool {
-        true
-    }
-}
-
-impl<Token: arch::Token, Scalar: scalar::Scalar<Token>> core::cmp::Eq
-    for TokenAlignment<Token, Scalar>
-{
-}
-
-impl<Token: arch::Token, Scalar: scalar::Scalar<Token>> core::cmp::PartialOrd
-    for TokenAlignment<Token, Scalar>
-{
-    #[inline]
-    fn partial_cmp(&self, _other: &Self) -> Option<core::cmp::Ordering> {
-        Some(core::cmp::Ordering::Equal)
-    }
-}
-
-impl<Token: arch::Token, Scalar: scalar::Scalar<Token>> core::cmp::Ord
-    for TokenAlignment<Token, Scalar>
-{
-    #[inline]
-    fn cmp(&self, _other: &Self) -> core::cmp::Ordering {
-        core::cmp::Ordering::Equal
-    }
-}
-
-impl<Token: arch::Token, Scalar: scalar::Scalar<Token>> core::hash::Hash
-    for TokenAlignment<Token, Scalar>
-{
-    #[inline]
-    fn hash<H: core::hash::Hasher>(&self, _: &mut H) {}
-}
+#[derive(Copy, Clone)]
+struct Vectors<Token: arch::Token, Scalar: scalar::Scalar<Token>>(
+    SizedVector<Scalar, width::W1, Token>,
+    SizedVector<Scalar, width::W2, Token>,
+    SizedVector<Scalar, width::W4, Token>,
+    SizedVector<Scalar, width::W8, Token>,
+);
 
 macro_rules! max_alignment {
     { $first:path, $($rest:path,)* } => {
-        /// A zero sized type with maximum possible vector alignment for a particular scalar on
-        /// the current architecture.
-        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+
+        #[doc(hidden)]
         #[repr(C)]
-        pub struct MaxAlignment<Scalar: scalar::Scalar<$first> $(+ scalar::Scalar<$rest>)*>(
-            TokenAlignment<$first, Scalar>,
-            $(TokenAlignment<$rest, Scalar>,)*
+        #[derive(Copy, Clone)]
+        pub struct AllVectors<Scalar: scalar::Scalar<$first> $(+ scalar::Scalar<$rest>)*>(
+            Vectors<$first, Scalar>,
+            $(
+            Vectors<$rest, Scalar>,
+            )*
         );
-
-        unsafe impl<Scalar: scalar::Scalar<$first> $(+ scalar::Scalar<$rest>)*> Alignment for MaxAlignment<Scalar> {}
-
-        impl<Scalar: scalar::Scalar<$first> $(+ scalar::Scalar<$rest>)*> core::fmt::Debug
-            for MaxAlignment<Scalar>
-        {
-            #[inline]
-            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-                f.pad("MaxAlignment")
-            }
-        }
 
         /// Allocate a boxed slice of scalars with maximum possible vector alignment for a
         /// particular scalar on the current architecture.
         ///
         /// # Panics
-        /// Panics if `size` is 0 or memory allocation fails.
+        /// Panics if `count` is 0 or memory allocation fails.
         #[cfg(any(feature = "std", feature = "alloc"))]
-        pub fn allocate_max_aligned_slice<Scalar: scalar::Scalar<$first> $(+ scalar::Scalar<$rest>)*>(size: usize) -> Box<[Scalar]> {
-            unsafe {
-                allocate_arbitrary_aligned_slice::<Scalar>(
-                    size,
-                    core::mem::align_of::<MaxAlignment<Scalar>>(),
-                )
-            }
+        pub fn allocate_max_aligned_slice<Scalar: Default + scalar::Scalar<$first> $(+ scalar::Scalar<$rest>)*>(count: usize) -> Box<[Scalar]> {
+            allocate_aligned_slice::<AllVectors<Scalar>, Scalar>(count)
         }
     }
 }
 
 crate::call_macro_with_tokens! { max_alignment }
 
-/// Indicates that a type represents an alignment.
-///
-/// # Safety
-/// The type must be zero sized.
-pub unsafe trait Alignment {}
-
-/// Aligns a value to a particular vector alignment.
-#[derive(Copy, Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+/// Aligns a value to another type's alignment.
 #[repr(C)]
-pub struct Aligned<Alignment: self::Alignment, T> {
-    _alignment: Alignment,
+pub struct Aligned<AlignTo, T> {
+    alignment: [AlignTo; 0],
     value: T,
 }
 
-impl<Alignment: self::Alignment, T> core::ops::Deref for Aligned<Alignment, T> {
+impl<AlignTo, T> Aligned<AlignTo, T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            alignment: [],
+            value,
+        }
+    }
+}
+
+impl<AlignTo, T> core::ops::Deref for Aligned<AlignTo, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -211,119 +71,98 @@ impl<Alignment: self::Alignment, T> core::ops::Deref for Aligned<Alignment, T> {
     }
 }
 
-impl<Alignment: self::Alignment, T> core::ops::DerefMut for Aligned<Alignment, T> {
+impl<AlignTo, T> core::ops::DerefMut for Aligned<AlignTo, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.value
     }
 }
 
-#[cfg(any(feature = "std", feature = "alloc"))]
-unsafe fn allocate_arbitrary_aligned_slice<T>(size: usize, align: usize) -> Box<[T]> {
-    assert!(size > 0, "size must be nonzero");
-    let layout = alloc::Layout::from_size_align(size * core::mem::size_of::<T>(), align).unwrap();
-    let ptr = alloc::alloc_zeroed(layout) as *mut T;
-    assert!(!ptr.is_null());
-    Box::from_raw(core::ptr::slice_from_raw_parts_mut(ptr, size))
-}
+impl<AlignTo: Copy, T: Copy> Copy for Aligned<AlignTo, T> {}
 
-/// Allocate a boxed slice of scalars with vector alignment.
-///
-/// # Panics
-/// Panics if `size` is 0 or memory allocation fails.
-#[cfg(any(feature = "std", feature = "alloc"))]
-pub fn allocate_aligned_slice<Token: arch::Token, Scalar: scalar::Scalar<Token>>(
-    size: usize,
-) -> Box<[Scalar]> {
-    unsafe {
-        allocate_arbitrary_aligned_slice::<Scalar>(
-            size,
-            core::mem::align_of::<TokenAlignment<Token, Scalar>>(),
-        )
+impl<AlignTo, T: Clone> Clone for Aligned<AlignTo, T> {
+    fn clone(&self) -> Self {
+        Self::new(self.value.clone())
     }
 }
+
+impl<AlignTo, T: Default> Default for Aligned<AlignTo, T> {
+    fn default() -> Self {
+        Self::new(T::default())
+    }
+}
+
+impl<AlignTo, T: core::fmt::Debug> core::fmt::Debug for Aligned<AlignTo, T> {
+    #[inline]
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_tuple("Aligned").field(&self.value).finish()
+    }
+}
+
+impl<AlignTo, T: core::cmp::PartialEq> core::cmp::PartialEq for Aligned<AlignTo, T> {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        self.value.eq(&other.value)
+    }
+}
+
+impl<AlignTo, T: core::cmp::Eq> core::cmp::Eq for Aligned<AlignTo, T> {}
+
+impl<AlignTo, T: core::cmp::PartialOrd> core::cmp::PartialOrd for Aligned<AlignTo, T> {
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<core::cmp::Ordering> {
+        self.value.partial_cmp(&other.value)
+    }
+}
+
+impl<AlignTo, T: core::cmp::Ord> core::cmp::Ord for Aligned<AlignTo, T> {
+    #[inline]
+    fn cmp(&self, other: &Self) -> core::cmp::Ordering {
+        self.value.cmp(&other.value)
+    }
+}
+
+impl<AlignTo, T: core::hash::Hash> core::hash::Hash for Aligned<AlignTo, T> {
+    #[inline]
+    fn hash<H: core::hash::Hasher>(&self, hasher: &mut H) {
+        self.value.hash(hasher)
+    }
+}
+
+/// Allocate a boxed slice of `count` `T`s aligned to the `AlignTo`.
+///
+/// # Panics
+/// Panics if `count` is 0 or memory allocation fails.
+#[cfg(any(feature = "std", feature = "alloc"))]
+pub fn allocate_aligned_slice<AlignTo, T: Default>(count: usize) -> Box<[T]> {
+    assert!(count > 0, "size must be nonzero");
+    let layout = alloc::Layout::from_size_align(
+        count * core::mem::size_of::<T>(),
+        core::cmp::max(core::mem::align_of::<AlignTo>(), core::mem::align_of::<T>()),
+    )
+    .unwrap();
+    unsafe {
+        let ptr = alloc::alloc(layout) as *mut T;
+        assert!(!ptr.is_null());
+        for i in 0..count {
+            ptr.add(i).write(T::default());
+        }
+        Box::from_raw(core::ptr::slice_from_raw_parts_mut(ptr, count))
+    }
+}
+
+/// Aligns a type to the maximum possible vector alignment for a particular scalar on the current
+/// architecture.
+pub type MaxAligned<Scalar, T> = Aligned<AllVectors<Scalar>, T>;
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    #[test]
-    fn check_zst() {
-        use crate::arch::generic::Generic;
-
-        // Alignment
-        assert_eq!(core::mem::size_of::<TokenAlignment<Generic, f32>>(), 0);
-        assert_eq!(core::mem::size_of::<TokenAlignment<Generic, f64>>(), 0);
-        #[cfg(feature = "complex")]
-        assert_eq!(
-            core::mem::size_of::<TokenAlignment<Generic, num_complex::Complex<f32>>>(),
-            0
-        );
-        #[cfg(feature = "complex")]
-        assert_eq!(
-            core::mem::size_of::<TokenAlignment<Generic, num_complex::Complex<f64>>>(),
-            0
-        );
-
-        // MaxAlignment
-        assert_eq!(core::mem::size_of::<MaxAlignment<f32>>(), 0);
-        assert_eq!(core::mem::size_of::<MaxAlignment<f64>>(), 0);
-        #[cfg(feature = "complex")]
-        assert_eq!(
-            core::mem::size_of::<MaxAlignment<num_complex::Complex<f32>>>(),
-            0
-        );
-        #[cfg(feature = "complex")]
-        assert_eq!(
-            core::mem::size_of::<MaxAlignment<num_complex::Complex<f64>>>(),
-            0
-        );
-    }
-
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     #[test]
     fn check_x86() {
-        use crate::arch::x86::{Avx, Sse};
-
-        // SSE
-        assert_eq!(core::mem::align_of::<TokenAlignment<Sse, f32>>(), 16);
-        assert_eq!(core::mem::align_of::<TokenAlignment<Sse, f64>>(), 16);
-        #[cfg(feature = "complex")]
-        assert_eq!(
-            core::mem::align_of::<TokenAlignment<Sse, num_complex::Complex<f32>>>(),
-            16
-        );
-        #[cfg(feature = "complex")]
-        assert_eq!(
-            core::mem::align_of::<TokenAlignment<Sse, num_complex::Complex<f64>>>(),
-            16
-        );
-
-        // AVX
-        assert_eq!(core::mem::align_of::<TokenAlignment<Avx, f32>>(), 32);
-        assert_eq!(core::mem::align_of::<TokenAlignment<Avx, f64>>(), 32);
-        #[cfg(feature = "complex")]
-        assert_eq!(
-            core::mem::align_of::<TokenAlignment<Avx, num_complex::Complex<f32>>>(),
-            32
-        );
-        #[cfg(feature = "complex")]
-        assert_eq!(
-            core::mem::align_of::<TokenAlignment<Avx, num_complex::Complex<f64>>>(),
-            32
-        );
-
-        // MaxTokenAlignment
-        assert_eq!(core::mem::align_of::<MaxAlignment<f32>>(), 32);
-        assert_eq!(core::mem::align_of::<MaxAlignment<f64>>(), 32);
-        #[cfg(feature = "complex")]
-        assert_eq!(
-            core::mem::align_of::<MaxAlignment<num_complex::Complex<f32>>>(),
-            32
-        );
-        #[cfg(feature = "complex")]
-        assert_eq!(
-            core::mem::align_of::<MaxAlignment<num_complex::Complex<f64>>>(),
-            32
-        );
+        type Foo = [f32; 8];
+        type AlignedFoo = MaxAligned<f32, Foo>;
+        assert_eq!(core::mem::align_of::<AlignedFoo>(), 32);
     }
 }
